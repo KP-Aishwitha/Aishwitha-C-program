a6987626c36d5b3a67b18c47d7c0774b33a807b3
KP-Aishwitha <aishwithakp@gmail.com>
2024-08-31 13:01:46 +0530

    PRIME NUMBER.c

---
+++ PRIME NUMBER.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int n;
+  printf("enter the number:");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+    printf("not a prime number");
+  }
+  else
+  {
+    printf("prime number");
+  }  
+}
