e55d1ff9f3ba8b355e79f68c2a1b4073175494a2
KP-Aishwitha <aishwithakp@gmail.com>
2024-08-31 13:41:30 +0530

    UNION.c

---
+++ UNION.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+union book
+{
+ int num;
+ char name[50];
+ float price;
+
+};
+int main()
+{
+  union book hp;
+  hp.num=1;
+  hp.price=100.0;
+  strcpy(hp.name,"Harry Potter");
+  printf("Book number: %d\n",hp.num);
+  printf("the book name is: %s\n",hp.name);
+  printf("book price %f\n",hp.price);
+}
