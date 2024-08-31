1c69acafcc4d115b59563d4a348ded6cf153197f
KP-Aishwitha <aishwithakp@gmail.com>
2024-08-31 12:46:35 +0530

    Create Fibonacci sequence.c

---
+++ Fibonacci sequence.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int fib(int n){
+if(n==0)
+{
+    return 0;
+}
+else if(n==1)
+{
+    return 1;
+}
+else
+{
+    return fib(n-1)+fib(n-2);
+}
+}
+void main()
+{
+    int n,fibo;
+    printf("enter the value of n\n");
+    scanf("%d",&n);
+    fibo=fib(n);
+    printf("fibonacci=%d",fibo);
+    
+}
