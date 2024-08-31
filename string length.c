425b1849420119fec78cefc714820258128b1e7d
KP-Aishwitha <aishwithakp@gmail.com>
2024-08-31 13:09:25 +0530

    string length.c

---
+++ string length.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char str[10],n;
+printf("enter the name:");
+scanf("%s",str);
+n=strlen(str)
+printf("HELLO %s,WELCOME TO C PROGRAMMING\n",str);
+printf("length of the string is %d",n);
+}
+
+                    
